b711a047891f8fccd9608e72e4eb1d47b638c690
Walmyr <wlsf82@gmail.com>
2021-07-20 20:50:38 +0200

    Add a few "features"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
+  const [isTextInputvisible, setIsTextInputvisible] = React.useState(false)
+  const [isParagraphVisible, setIsParagraphVisible] = React.useState(false)
+  const [paragraphText, setParagraphText] = React.useState('')
+
+  const handleClick = () => {
+    setIsTextInputvisible(true)
+  }
+
+  const handleChange = (event) => {
+    setIsParagraphVisible(true)
+    if (event.target.value.toLowerCase() === 'secret') {
+      setParagraphText('Congrats, you found it!')
+    } else {
+      setParagraphText(event.target.value)
+    }
+  }
+
   return (
     <div className="App">
       <h1>Live TAT Code Coverage</h1>
+      <button onClick={handleClick}>Start</button>
+
+      { isTextInputvisible && <input type="text" placeholder="Find the easter egg" onChange={handleChange} /> }
+      { isParagraphVisible && <p>{paragraphText}</p> }
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
