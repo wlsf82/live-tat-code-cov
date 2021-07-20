aa05051afed7d6167a03716cf724a4809637d430
Walmyr <wlsf82@gmail.com>
2021-07-20 20:51:55 +0200

    Cover new features with tests

--- cypress/integration/live-tat-code-coverage.spec.js
+++ cypress/integration/live-tat-code-coverage.spec.js
@@ -1,4 +1,32 @@
-it('has a "Live TAT Code Coverage" h1', () => {
-  cy.visit('/');
-  cy.get('h1').should('contain', 'Live TAT Code Coverage');
+describe('Live TAT Code Coverage', () => {
+  it('shows elements while interacting with them', () => {
+    cy.visit('/')
+
+    cy.contains('h1', 'Live TAT Code Coverage').should('be.visible')
+
+    cy.contains('button', 'Start')
+      .click()
+
+    cy.get('input[type="text"]')
+      .as('textField')
+      .should('be.visible')
+      .type('Hello world!')
+
+    cy.contains('p', 'Hello world!')
+      .should('be.visible')
+
+    cy.get('@textField')
+      .clear()
+      .type('secret')
+
+    cy.contains('p', 'Congrats, you found it!')
+      .should('be.visible')
+
+    cy.get('@textField')
+      .clear()
+      .type('SECRET')
+
+    cy.contains('p', 'Congrats, you found it!')
+      .should('be.visible')
+  })
 })
