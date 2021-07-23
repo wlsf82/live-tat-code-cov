a29d58bf6d149eb139585e8614a0fa13f4d94445
Walmyr <wlsf82@gmail.com>
2021-07-23 22:58:30 +0200

    wip: Add reset feature and test it out
    This is still a work-in-progress since there's commented code on
    tests. It seems to be a Cypress issue, where it references to the
    wrong alised element.

--- cypress/integration/live-tat-code-coverage.spec.js
+++ cypress/integration/live-tat-code-coverage.spec.js
@@ -5,7 +5,9 @@ describe('Live TAT Code Coverage', () => {
     cy.contains('h1', 'Live TAT Code Coverage').should('be.visible')
 
     cy.contains('button', 'Start')
+      .as('startBtn')
       .click()
+      .should('be.disabled')
 
     cy.get('input[type="text"]')
       .as('textField')
@@ -21,12 +23,30 @@ describe('Live TAT Code Coverage', () => {
 
     cy.contains('p', 'Congrats, you found it!')
       .should('be.visible')
+    cy.contains('button', 'Reset')
+      .as('resetBtn')
+      .should('be.visible')
 
     cy.get('@textField')
       .clear()
+    cy.get('@resetBtn')
+      .should('not.exist')
+    cy.get('@textField')
       .type('SECRET')
 
     cy.contains('p', 'Congrats, you found it!')
       .should('be.visible')
+    cy.get('@resetBtn')
+      .should('be.visible')
+      .click()
+
+    cy.get('@startBtn')
+      .should('be.enabled')
+    cy.get('@textField')
+      .should('not.exist')
+    cy.contains('p', 'Congrats, you found it!')
+      .should('not.exist')
+    // cy.get('@resetBtn')
+    //   .should('not.exist')
   })
 })
--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const [isTextInputVisible, setIsTextInputVisible] = React.useState(false)
   const [isParagraphVisible, setIsParagraphVisible] = React.useState(false)
   const [paragraphText, setParagraphText] = React.useState('')
+  const [isResetLinkVisible, setIsResetLinkVisible] = React.useState(false)
 
   const handleClick = () => {
     setIsTextInputVisible(true)
@@ -14,18 +15,28 @@ function App() {
     setIsParagraphVisible(true)
     if (event.target.value.toLowerCase() === 'secret') {
       setParagraphText('Congrats, you found it!')
+      setIsResetLinkVisible(true)
     } else {
       setParagraphText(event.target.value)
+      setIsResetLinkVisible(false)
     }
+  }
+
+  const handleReset = () => {
+    setIsTextInputVisible(false)
+    setIsParagraphVisible(false)
+    setParagraphText('')
+    setIsResetLinkVisible(false)
   }
 
   return (
     <div className="App">
       <h1>Live TAT Code Coverage</h1>
-      <button onClick={handleClick}>Start</button>
+      <button onClick={handleClick} disabled={isTextInputVisible}>Start</button>
 
       { isTextInputVisible && <input type="text" placeholder="Find the easter egg" onChange={handleChange} /> }
       { isParagraphVisible && <p>{paragraphText}</p> }
+      { isResetLinkVisible && <button onClick={handleReset}>Reset</button> }
     </div>
   )
 }
